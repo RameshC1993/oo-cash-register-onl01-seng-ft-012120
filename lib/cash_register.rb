f41573fbe5df21dd343d2f279d3fbd550db4cd95
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-02-04 00:26:16 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,7 +4,7 @@ class CashRegister
   def initialize(discount = 0)
     @total = 0 
     @discount = discount 
-    #@last_transaction = last_transaction
+    @last_transaction = {}
     @items = []
   end
   
